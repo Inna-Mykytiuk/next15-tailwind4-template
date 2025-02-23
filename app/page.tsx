0c7869198a9029abfb4919c9231250e22a624321
Inna Mykytiuk <innka-pinnka@ukr.net>
2025-02-23 15:30:37 +0200

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1 className="text-3xl font-bold text-red-500">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+
